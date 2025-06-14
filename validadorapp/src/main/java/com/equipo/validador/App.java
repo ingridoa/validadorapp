e11459a905e37095a4709c5ec95355c58abbc1e0
ingridonate <ingrid.oa@gmail.com>
2025-06-14 16:24:21 -0400

    cambiodeversion sonarqube

--- validadorapp/src/main/java/com/equipo/validador/App.java
+++ validadorapp/src/main/java/com/equipo/validador/App.java
@@ -2,9 +2,8 @@ package com.equipo.validador;
 
 public class App {
     public static void main(String[] args) {
-        String usuario = "admin";
-        String contraseña = "123456"; // Hardcoded credentials
-        if (usuario.equals("admin")) {
+        String usuario = System.getenv("APP_USER");
+        if ("admin".equals(usuario)) {
             System.out.println("¡Bienvenido administrador!");
         }
     }
